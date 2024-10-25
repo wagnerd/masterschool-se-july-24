7fe2439b06b61ba25f7efcd02a60aa8e1b721338
Daniel Wagner <daniel.wagner@sewts.de>
2024-10-25 08:04:03 +0200

    Changes from 18th

--- unit_102/sprint_102_1/functions.py
+++ unit_102/sprint_102_1/functions.py
@@ -2,6 +2,7 @@
 Shared functions from sprint 102.1
 """
 
+
 def function_102_1():
 
     print("I'm from 102.1")
--- unit_103/sprint_103_2/functions.py
+++ unit_103/sprint_103_2/functions.py
@@ -2,11 +2,13 @@
 Contains all functions for sprint 103.2
 """
 
-def function_1():
+
+def function_1(a: bool, b: str) -> bool:
     """
     Does something
-
+    :arg:
+        :a: What is a
+        :b
     :return: True always
     """
-
-    return True
+    return a
